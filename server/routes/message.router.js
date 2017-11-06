87abccf67e356be4d2d9c62dcaca619d1e4b4dcf
Marta Jopp <martapeterson@gmail.com>
2017-11-06 14:24:15 -0600

    mongoose setup

--- server/routes/message.router.js
+++ server/routes/message.router.js
@@ -1,8 +1,17 @@
 var express = require ('express');
 var router = express.Router();
 
-router.post('/', function(req, res){
-    console.log(req.body);
-});
+router.post('/', function (req, res) {
+    console.log(req.body); //message and username
+    var messageToAdd = new Message(req.body); //renaming to work with mongoose
+    messageToAdd.save(function (err, data) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            res.sendStatus(201);
+        }
+    }); // END SAVE
+}); // END POST Route
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,10 +2,25 @@ var express = require ('express');
 var app = express();
 var port = 5000;
 var bodyParser = require('body-Parser');
+var messageRouter = require('./routes/message.router.js');
+var mongoose = require('mongoose');
+var databaseUrl = 'mongodb://localhost:27017/messageboard';
+
 
 app.use(bodyParser.json());
 
 app.use(express.static('server/public'));
+app.use('/messages', messageRouter);
+
+
+mongoose.connection.on('connected', function () {
+    console.log('mongoose is connected!'); //lets us know mongoose is connected
+});
+
+mongoose.connection.on('error', function () {
+    console.log('mongoose connection failed');
+});
+mongoose.connect(databaseUrl);
 
 app.listen(port, function(){
     console.log('listening', port);
